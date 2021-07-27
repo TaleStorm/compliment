62b066edc217eec9873f8601d168440a867741a6
evgent007 <57885856+evgent007@users.noreply.github.com>
2021-07-27 15:35:37 +0700

    Update data_manager.py

--- data_manager.py
+++ data_manager.py
@@ -11,13 +11,33 @@ class DataManager:
         Session.configure(bind=self.engine)
         self.session = Session()
 
-    def create_table(self, obj):
+    def _create_table(self, obj):
         obj.metadata.create_all(self.engine)
 
     def add(self, obj):
         self.session.add(obj)
         self.session.commit()
 
-    def getById(self, obj, id):
-        for result in self.session.query(obj).filter_by(id=id):
-            return result
+    def get_by_id(self, obj, id):
+        """
+        Возвращает объект по id
+        """
+        result = self.session.query(obj).get(id)
+        return result
+
+    def del_by_id(self, obj, id):
+        self.session.delete(self.session.query(obj).get(id))
+        self.session.commit()
+
+    def del_obj(self, obj):
+        self.session.delete(obj)
+        self.session.commit()
+
+    def get_by_all(self,obj):
+        for ins in self.session.query(obj).order_by(obj.id):
+            print(ins)
+            print (ins.name, ins.id, ins.chat_id)
+
+    def update(self, obj, value):
+        self.session.query(obj).update(value)
+
