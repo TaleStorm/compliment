3a0ceda30ca71d0bd2d536a4118d8eb3798d12ef
evgent <evgent007@gmail.com>
2021-07-29 11:10:38 +0700

    создал папку db

---
+++ db/botdb.py
@@ -0,0 +1,74 @@
+from example import User, ContactsUser
+from loguru import logger
+from dotenv import load_dotenv
+from aiogram.utils import executor
+from aiogram.dispatcher.filters import Text
+from aiogram.dispatcher.middlewares import LifetimeControllerMiddleware
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram import Bot, Dispatcher
+import aioredis
+from data_manager import DataManager
+from random import randint
+from datetime import datetime as dt
+import os
+import asyncio
+
+
+# class RedisMiddleware(LifetimeControllerMiddleware):
+#     def __init__(self):
+#         super().__init__()
+
+#     async def pre_process(self, obj, data, *args):
+#         pool = await aioredis.create_pool(
+#             "redis://localhost",
+#             encoding='utf-8'
+#         )
+#         data['redis'] = aioredis.Redis(pool_or_conn=pool)
+
+
+# class KeySchema:
+#     """
+#     Возвращает название таблиц для Redis.
+#     """
+
+#     def user_messages_key(self, chat_id):
+#         """Хэш-таблица с сообщениями."""
+#         return f'user:list:{chat_id}'
+
+#     def users_set(self):
+#         """Список юзеров, использующих бота."""
+#         return 'users:set'
+
+#     def user_info(self, chat_id):
+#         """Хэш-информация о юзере."""
+#         return f'user:info:{chat_id}'
+
+
+pool = aioredis.create_pool("redis://localhost", encoding='utf-8')
+redis = aioredis.Redis(pool_or_conn=pool)
+print('data', type(redis))
+
+
+def db_add_user(message, state):
+    """
+    Заполнение таблицы users
+    """
+
+    with state.proxy() as data:
+
+        user = User(data['name'], data['birthday'], message.chat.id)
+        data_manager.add(user)
+
+
+data_manager = DataManager('sqlite:///sqlite3.db')
+data_manager._create_table(User)
+data_manager._create_table(ContactsUser)
+
+us = data_manager.get_by_id(User, 23)
+print(us)
+#data_manager.del_obj(us)
+
+data_manager.update_state(User, 21, 'birthday', '12-31-23')
+data_manager.update_state(User, 21, 'chat_id', 4555969)
+data_manager.get_by_all(User)
---
+++ db/data_manager.py
@@ -0,0 +1,60 @@
+from sqlalchemy import create_engine
+from datetime import datetime
+from sqlalchemy import MetaData, Table, String, Integer, Column, Text, DateTime, Boolean
+from sqlalchemy.orm import sessionmaker
+
+
+class DataManager:
+    def __init__(self, str_connection):
+        self.engine = create_engine(str_connection)
+        self.engine.connect()
+        Session = sessionmaker()
+        Session.configure(bind=self.engine)
+        self.session = Session()
+
+    def _create_table(self, obj):
+        obj.metadata.create_all(self.engine)
+
+    def add(self, obj):
+        self.session.add(obj)
+        self.session.commit()
+
+    def get_by_id(self, obj, id):
+        """
+        Возвращает объект по id
+        """
+        result = self.session.query(obj).get(id)
+        return result
+
+    def del_by_id(self, obj, id):
+        '''
+        Удалчет объект по id
+        '''
+        self.session.delete(self.session.query(obj).get(id))
+        self.session.commit()
+
+    def del_obj(self, obj):
+        self.session.delete(obj)
+        self.session.commit()
+
+    def update(self, obj, value):
+        '''неработает'''
+        self.session.query(obj).update(value)
+        self.session.commit()
+
+    def update_state(self, obj, id, state, new):
+        '''
+        Изменяет значение атрибута state на new
+        '''
+        try:
+            user = self.session.query(obj).get(id)
+            setattr(user, state, new)
+            self.session.commit()
+        except:
+            print('Error in def update_state')
+
+    def get_by_all(self, obj):
+        for ins in self.session.query(obj).order_by(obj.id):
+            print(ins)
+
+        print(self.session.query(obj).all())
