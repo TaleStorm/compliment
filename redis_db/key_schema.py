82179e9f51c825686880f7c613376fbe5d7ec663
Dolise <dolise@yandex.ru>
2021-08-20 12:22:16 +0300

    redis keyschema upd

--- redis_db/key_schema.py
+++ redis_db/key_schema.py
@@ -10,6 +10,21 @@ class KeySchema:
         """Список юзеров, использующих бота."""
         return 'users:set'
 
+    def contact_messages(self, user_chat_id, contact_username):
+        return f'hash:messages:{user_chat_id}:{contact_username}'
+
     def user_info(self, chat_id):
         """Хэш-информация о юзере."""
         return f'user:info:{chat_id}'
+
+    def check_contact_status(self):
+        return 'hash:check_contact_status'
+
+    def check_contact(self):
+        return 'list:check_contact'
+
+    def conf_code_by_id(self):
+        return 'hash:id_conf_code'
+
+    def code_entered(self):
+        return 'set:code_entered'
